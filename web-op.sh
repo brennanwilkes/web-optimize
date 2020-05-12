651a194b245557e83d08ee3d6d5ce70108ec4191
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-11 19:14:31 -0700

    Quoting

--- web-op.sh
+++ web-op.sh
@@ -7,14 +7,15 @@ SCALEFACT=100
 OUTPUTFILE='${fileSTRIPPED}-web.jpg'
 SATURATE=100
 OUTPUT_MD=0
+SCRIPTNAME=$( echo -n "$0" | grep -o '[^/]*$' )
 
 #Usage message
 print_err(){
-	>&2 echo "usage: $0 [-s] SCALE_FACTOR [-o] OUTPUT_DIRECTORY [-S] AMT [FILES...]
+	>&2 echo "usage: "$SCRIPTNAME" [-s] SCALE_FACTOR [-o] OUTPUT_DIRECTORY [-S] AMT [FILES...]
 
 	-s Scale factor to compress images by
-		ex. $0 -s 50 - will scale a 1000x1800 image to 500x900
-		ex. $0 -s 25 - will scale a 1000x1800 image to 250x450
+		ex. "$SCRIPTNAME" -s 50 - will scale a 1000x1800 image to 500x900
+		ex. "$SCRIPTNAME" -s 25 - will scale a 1000x1800 image to 250x450
 
 	-o Write files to an output directory.
 
@@ -23,8 +24,8 @@ print_err(){
 
 #Function to check valid arguments
 check_args(){
-	[ $# -lt 1 ] && {
-		>&2 echo "invalid Aguments - "
+	[ "$#" -lt 1 ] && {
+		>&2 echo "invalid Aguments"
 		print_err
 		exit 1
 	};
@@ -34,57 +35,57 @@ check_args(){
 for arg in $(seq 4); do
 
 	#check arguments
-	check_args $@
+	check_args "$@"
 
 	#Scale argument
-	[ $1 = "-s" ] && {
+	[ "$1" = "-s" ] && {
 		shift
 
 		#Check for follow up arguments
-		[ $1 -eq $1 ] 2>/dev/null &&{
-			check_args $@
+		[ "$1" -eq "$1" ] 2>/dev/null &&{
+			check_args "$@"
 
 			#set scale factor
-			SCALEFACT=$1
+			SCALEFACT="$1"
 			shift
 			continue
 		} || {
-			>&2 echo "Invalid scale factor \"$1\" - "
+			>&2 echo "Invalid scale factor \""$1"\""
 			print_err
 			exit 1
 		}
 	};
 
 	#Output directory argument
-	[ $1 = "-o" ] && {
+	[ "$1" = "-o" ] && {
 		shift
-		[ -d $1 ] 2>/dev/null &&{
-			check_args $@
+		[ -d "$1" ] 2>/dev/null &&{
+			check_args "$@"
 
 			#set output variables
 			OUTPUT_MD=1
-			OUTPUTPATH=$1
+			OUTPUTPATH="$1"
 			OUTPUTFILE='${OUTPUTPATH}${fileSTRIPPED}.jpg'
 			shift
 		} || {
-			>&2 echo "Invalid Path \"$1\" - "
+			>&2 echo "Invalid Path \""$1"\""
 			print_err
 			exit 1
 		}
 	};
 
 	#Saturation argument
-	[ $1 = "-S" ] && {
+	[ "$1" = "-S" ] && {
 		shift
-		[ $1 -eq $1 ] 2>/dev/null &&{
-			check_args $@
+		[ "$1" -eq "$1" ] 2>/dev/null &&{
+			check_args "$@"
 
 			#Saturation factor
-			SATURATE=$1
+			SATURATE="$1"
 			shift
 			continue
 		} || {
-			>&2 echo "Invalid saturation factor \"$1\" - "
+			>&2 echo "Invalid saturation factor \""$1"\""
 			print_err
 			exit 1
 		}
@@ -102,7 +103,7 @@ temp_file="${temp_file}.jpg"
 for file in "$@"; do
 
 	#Ensure that file exists
-	[ -f $file ] && {
+	[ -f "$file" ] && {
 
 		#Strip away file extension
 		fileSTRIPPED=$(echo -n "$file" | sed 's/\.[^.]*$//')
@@ -116,10 +117,10 @@ for file in "$@"; do
 		eval echo "converting $file to $OUTPUTFILE at $SCALEFACT% scale and $SATURATE% saturation"
 
 		#Scale image using ImageMagick
-		convert $file -resize "$SCALEFACT%" "$temp_file"
+		convert "$file" -resize "$SCALEFACT%" "$temp_file"
 
 		#Saturate image using ImageMagick
-		[ $SATURATE -ne 100 ] && {
+		[ "$SATURATE" -ne 100 ] && {
 			convert "$temp_file" -modulate 100,"$SATURATE",100 "$temp_file"
 		}
 
@@ -128,7 +129,7 @@ for file in "$@"; do
 
 	#Debug info
 	} || {
-		echo "invalid image \"$file\". Skipping..."
+		echo "invalid image \""$file"\". Skipping..."
 	};
 done
 
